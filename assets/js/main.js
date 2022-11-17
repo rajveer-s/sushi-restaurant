edd2db4eb3a17e97a11af52ce3cbeb0c0042278c
Rajveer Singh <rajsidhu2924@gmail.com>
2022-11-16 16:26:38 -0800

    shjow menu function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+navToggle = document.getElementById('nav-toggle');
+navClose = document.getElementById('nav-close');
 
+// Menu Show 
+// validate if constant exists
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
+}
 
+// Menu Hidden 
+// validate if constant exists
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
@@ -8,13 +25,13 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
