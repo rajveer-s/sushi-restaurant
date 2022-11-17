56d20fef36cb5a1a6044e3df928c744f57030620
Rajveer Singh <rajsidhu2924@gmail.com>
2022-11-17 13:48:55 -0800

    show scroll function and css fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,20 +34,39 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const scrollHeader = () => {
   const Header = document.getElementById('header');
   // when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-  this.scrollY >= 50 ? Header.classList.add('bg-header') : Header.class.remove('bg-header');
+  this.scrollY >= 50 ? Header.classList.add('bg-header') : Header.classList.remove('bg-header');
 }
 window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
-  const scrollUp = document.getElementById('scroll-up');
+  const scrollUpId = document.getElementById('scroll-up');
   // When the scroll is higher than 350 viewport height, add teh show-scroll class to the a tag with the scrollUp
-  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+  this.scrollY >= 350 ? scrollUpId.classList.add('show-scroll') : scrollUpId.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// const sections = document.querySelectorAll('section[id]');
+
+// const scrollActive = () => {
+//   const scrollY = window.pageYOffset
+
+//   sections.forEach(current => {
+//     const sectionHeight = current.offsetHeight,
+//       sectionTop = current.offsetTop - 58,
+//       sectionId = current.getAttribute('id'),
+//       sectionClass = document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']')
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       sectionClass.classList.add('active-link')
+//     } else {
+//       sectionClass.classList.remove('active-link')
+//     }
+//   })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/
 
